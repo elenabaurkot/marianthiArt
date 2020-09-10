d5e8550f30832b403a1156b499a641b35d17c80a
elenabaurkot <elenabaurkot@gmail.com>
2020-09-10 16:47:46 -0400

    updated edits

--- script.js
+++ script.js
@@ -4,13 +4,10 @@ var path = window.location.href;
 // function to highlight the active nav-link in the navbar
 function navActive() {
   for (let i = 0; i < navLink.length; i++) {
-    console.log(i);
-    console.log(navLink[i]);
     if (navLink[i].href === path) {
       navLink[i].classList.add('active');
     }
   }
 }
-
 // call the navActive function on load
 document.addEventListener('DOMContentLoaded', navActive(), false);
